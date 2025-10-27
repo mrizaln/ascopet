68c5a876df74f6dac71838cf333fd7e996356c15
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-10-27 17:25:41 +0700

    Update example

--- example/source/trace.cpp
+++ example/source/trace.cpp
@@ -55,24 +55,33 @@ void producer(std::stop_token st, ascopet::Duration duration, std::string_view n
 
 void contention(std::atomic<bool>& flag, std::size_t count, std::string_view name)
 {
-    auto start = Clock::now();
     flag.wait(false);
+
+    auto start = Clock::now();
 
     std::println(">> start {}", name);
     for (auto i = 0u; i < count; ++i) {
         auto trace = ascopet::trace(name);    // timing overhead
     }
-    using Ms  = std::chrono::duration<double, std::milli>;
-    auto time = [&] { return std::chrono::duration_cast<Ms>(Clock::now() - start); };
-    std::println(">> end {} in {}", name, time());
+
+    using Ms   = std::chrono::duration<double, std::milli>;
+    auto to_ms = [&](auto dur) { return std::chrono::duration_cast<Ms>(dur); };
+
+    auto duration = Clock::now() - start;
+    std::println(">> end {} in {} ({}/iter)", name, to_ms(duration), duration / count);
 }
 
-void contention_test()
+void single_test(std::size_t count)
 {
-    auto ascopet = ascopet::instance();
-    auto flag    = std::atomic<bool>{ false };
+    auto flag = std::atomic<bool>{ true };
+    contention(flag, count, "single_test");
+}
 
+void contention_test(std::size_t count)
+{
     {
+        auto flag = std::atomic<bool>{ false };
+
         // auto thread1 = std::jthread{ producer, 10ms, "1" };
         // auto thread2 = std::jthread{ producer, 11ms, "2" };
         // auto thread3 = std::jthread{ producer, 12ms, "3" };
@@ -80,27 +89,26 @@ void contention_test()
         // auto thread5 = std::jthread{ producer, 10ms, "5" };
         // auto thread6 = std::jthread{ producer, 11ms, "6" };
 
-        auto thread7  = std::jthread{ contention, std::ref(flag), 10'024'000, "contention1" };
-        auto thread8  = std::jthread{ contention, std::ref(flag), 10'024'000, "contention2" };
-        auto thread9  = std::jthread{ contention, std::ref(flag), 10'024'000, "contention3" };
-        auto thread10 = std::jthread{ contention, std::ref(flag), 10'024'000, "contention4" };
-        auto thread11 = std::jthread{ contention, std::ref(flag), 10'024'000, "contention5" };
-        auto thread12 = std::jthread{ contention, std::ref(flag), 10'024'000, "contention6" };
+        auto thread7  = std::jthread{ contention, std::ref(flag), count, "contention1" };
+        auto thread8  = std::jthread{ contention, std::ref(flag), count, "contention2" };
+        auto thread9  = std::jthread{ contention, std::ref(flag), count, "contention3" };
+        auto thread10 = std::jthread{ contention, std::ref(flag), count, "contention4" };
+        auto thread11 = std::jthread{ contention, std::ref(flag), count, "contention5" };
+        auto thread12 = std::jthread{ contention, std::ref(flag), count, "contention6" };
+
+        std::this_thread::sleep_for(500ms);
 
         flag.store(true);
         flag.notify_all();
-
-        std::this_thread::sleep_for(250ms);
-
-        // record capacity can be changed on the fly, but buffer capacity cannot
-        ascopet->resize_record_capacity(512);
-
-        std::this_thread::sleep_for(1s);
     }
 
-    std::println("\ncontention_test:");
-    for (const auto& [id, traces] : ascopet->report()) {
-        print_report<std::chrono::nanoseconds>(id, traces);
+    if (auto ascopet = ascopet::instance(); ascopet and ascopet->is_tracing()) {
+        std::println("\ncontention_test:");
+        for (const auto& [id, traces] : ascopet->report()) {
+            print_report<std::chrono::nanoseconds>(id, traces);
+        }
+    } else {
+        std::println("\ncontention_test: not initialized or not tracing");
     }
 }
 
@@ -134,18 +142,41 @@ void sleep_test()
 
 int main()
 {
+    static constexpr auto count = 10'240'000uz;
+
+    std::println("\n{:-^80}", "uninitialized");
+    single_test(count);
+    std::println("");
+    contention_test(count);
+
+    std::println("\n{:-^80}", "init");
     auto ascopet = ascopet::init({
         .immediately_start = true,
         .poll_interval     = 25ms,
-        .buffer_capacity   = 10240,
+        .record_capacity   = 10240,    // per-label buffer; got collected from tls buffer every poll_interval
+        .buffer_capacity   = 10240,    // per-thread buffer (tls) caching trace data on each thread
     });
 
+    // record capacity can be resized on-the-fly. buffer capacity can't be resized
+    ascopet->resize_record_capacity(512);
+
+    // trace data are recorded using rdtsc, which you can check the frequency using this
     std::println(
-        "tsc_freq: {} Hz ({} MHz)\n",
+        "tsc_freq: {} Hz ({} MHz)",
         ascopet->tsc_freq(),
-        static_cast<float>(ascopet->tsc_freq()) / std::micro::den
+        static_cast<float>(ascopet->tsc_freq()) / std::mega::num
     );
 
-    contention_test();
-    sleep_test();
+    std::println("\n{:-^80}", "paused");
+    ascopet->pause_tracing();
+    single_test(count);
+    std::println("");
+    contention_test(count);
+
+    std::println("\n{:-^80}", "running");
+    ascopet->start_tracing();
+    single_test(count);
+    std::println("");
+    contention_test(count);
+    // sleep_test();
 }
